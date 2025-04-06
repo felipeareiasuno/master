62f13d57c314843b38d42e8d0ede6c66131c1941
felipeareiasuno <felipe.areia@suno.com.br>
2025-04-06 19:47:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ with abas[2]:
         st.markdown(f"**Pontos usados:** {pontos_usados}")
         st.markdown(f"**Pontos restantes:** {pontos - pontos_usados}")
         if saldo_a_pagar > 0:
-            st.markdown(f"**Saldo a pagar:** R$ {saldo_a_pagar:,.2f}".replace('.', ','))
+            st.markdown(f"**Saldo a pagar:st.markdown(f"**Saldo a pagar:** R$ {saldo_a_pagar:,.2f}".replace('.', ','))
     else:
         st.markdown("_Nenhuma recompensa selecionada ainda._")
     st.markdown("---")
